60ba1ca32061b3b8fe43386fd988e694634c9b7e
Sampaguitas <timothee.desurmont@sampaguitas.com>
2019-01-05 11:22:57 +0400

    changed output path ./dist

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: './src/index.jsx',
     output: {
-        path: path.resolve('dist'),
+        path: './dist',
         filename: 'bundle.js'
     },
     resolve: {
