7b8c1566421d0bf1ca2e2d6af1a3f3946670bcee
Timothee Desurmont <timothee.desurmont@sampaguitas.com>
2019-05-11 15:05:49 +0400

    change

--- src/_services/index.js
+++ src/_services/index.js
@@ -1,6 +1,3 @@
-export * from './currency.service';
-export * from './customer.service';
-export * from './incoterm.service';
 export * from './opco.service';
 export * from './project.service';
 export * from './user.service';
