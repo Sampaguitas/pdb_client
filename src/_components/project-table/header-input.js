0a5851a85d352a5a3d3819a88a73084c14bb1d0a
Timothee Desurmont <timothee.desurmont@sampaguitas.com>
2020-02-19 15:55:17 +0400

    changes

--- src/_components/project-table/header-input.js
+++ src/_components/project-table/header-input.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class HeaderInput extends Component{
 
@@ -18,12 +19,8 @@ class HeaderInput extends Component{
             <th style={{width: `${width ? width : 'auto'}`, whiteSpace: `${textNoWrap ? 'nowrap' : 'auto'}` }}>
                 
                 <div className="form-group" style={{marginBottom: '0px'}}>
-                    <label
-                        htmlFor={name}
-                        style={{marginBottom: '0px'}}
-                    >
-                        {title}
-                    </label>
+                    <label htmlFor={name} style={{marginBottom: '0px'}}>{title}</label>
+                    {/* <FontAwesomeIcon icon="sort-down" className="fa-2x ml-2"/> */}
                     <input
                         className="form-control"
                         // id={name}
@@ -31,7 +28,7 @@ class HeaderInput extends Component{
                         type={type}
                         value={value}
                         onChange={onChange}
-                        style={{padding: '.375rem .75rem'}}
+                        style={{padding: '.375rem .75rem'}} //{{padding: '0rem 0.75rem', lineHeight: '20px', height: '22px'}}//
                     />
                 </div>
 
