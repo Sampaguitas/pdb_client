75d1329d5d90ff428ab5fd7d1493fdcf19e7352d
Sampaguitas <timothee.desurmont@sampaguitas.com>
2020-08-06 14:01:51 +0400

    changes

--- src/pages/project/warehouse/materialissuerecord.jsx
+++ src/pages/project/warehouse/materialissuerecord.jsx
@@ -96,11 +96,6 @@ function getBodys(mirs, headersForShow) {
             objectRow  = {
                 _id: i,
                 tablesId: {
-                    poId: '',
-                    subId: '',
-                    certificateId: '',
-                    packitemId: '',
-                    collipackId: '',
                     mirId: mir._id,
                 },
                 fields: arrayRow
@@ -204,7 +199,6 @@ class MaterialIssueRecord extends React.Component {
         };
         this.handleClearAlert = this.handleClearAlert.bind(this);
         this.toggleUnlock = this.toggleUnlock.bind(this);
-        this.downloadTable = this.downloadTable.bind(this);
         this.handleChange = this.handleChange.bind(this);
         this.handleChangeNewMir = this.handleChangeNewMir.bind(this);
         this.handleCheckWarehouse = this.handleCheckWarehouse.bind(this);
@@ -238,7 +232,8 @@ class MaterialIssueRecord extends React.Component {
         this.fileInput = React.createRef();
         this.onKeyPress = this.onKeyPress.bind(this);
         this.toggleModalUpload = this.toggleModalUpload.bind(this);
-        this.handleUploadFile = this.handleUploadFile.bind(this);
+        this.downloadTable = this.downloadTable.bind(this);
+        this.uploadTable = this.uploadTable.bind(this);
         this.handleFileChange = this.handleFileChange.bind(this);
         this.generateRejectionRows = this.generateRejectionRows.bind(this);
     }
@@ -491,53 +486,6 @@ class MaterialIssueRecord extends React.Component {
             unlocked: !unlocked
         }, () => {
         });
-    }
-
-    downloadTable(event){
-        event.preventDefault();
-        const { projectId, screenId, screen, selectedIds, unlocked } = this.state;
-        if (_.isEmpty(selectedIds)) {
-            this.setState({
-                alert: {
-                    type: 'alert-danger',
-                    message: 'Select line(s) to be downloaded.'
-                }
-            });
-        } else if (projectId && screenId && screen) {
-            this.setState({
-                downloadingTable: true
-            }, () => {
-                var currentDate = new Date();
-                var date = currentDate.getDate();
-                var month = currentDate.getMonth();
-                var year = currentDate.getFullYear();
-                const requestOptions = {
-                    method: 'POST',
-                    headers: { ...authHeader(), 'Content-Type': 'application/json'},
-                    body: JSON.stringify({selectedIds: selectedIds})
-                };
-                return fetch(`${config.apiUrl}/extract/download?projectId=${projectId}&screenId=${screenId}&unlocked=${unlocked}`, requestOptions)
-                // .then(res => res.blob()).then(blob => saveAs(blob, `DOWNLOAD_${screen}_${year}_${baseTen(month+1)}_${date}.xlsx`));
-                .then(responce => {
-                    if (responce.status === 401) {
-                        localStorage.removeItem('user');
-                        location.reload(true);
-                    } else if (responce.status === 400) {
-                        this.setState({
-                            downloadingTable: false,
-                            alert: {
-                                type: 'alert-danger',
-                                message: 'an error has occured'  
-                            }
-                        });
-                    } else {
-                        this.setState({
-                            downloadingTable: false
-                        }, () => responce.blob().then(blob => saveAs(blob, `DOWNLOAD_${screen}_${year}_${baseTen(month+1)}_${date}.xlsx`)));
-                    }
-                });
-            });
-        }
     }
 
     handleChange(event) {
@@ -935,7 +883,54 @@ class MaterialIssueRecord extends React.Component {
         });
     }
 
-    handleUploadFile(event){
+    downloadTable(event){
+        event.preventDefault();
+        const { projectId, screenId, screen, selectedIds, unlocked } = this.state;
+        if (_.isEmpty(selectedIds)) {
+            this.setState({
+                alert: {
+                    type: 'alert-danger',
+                    message: 'Select line(s) to be downloaded.'
+                }
+            });
+        } else if (projectId && screenId && screen) {
+            this.setState({
+                downloadingTable: true
+            }, () => {
+                var currentDate = new Date();
+                var date = currentDate.getDate();
+                var month = currentDate.getMonth();
+                var year = currentDate.getFullYear();
+                const requestOptions = {
+                    method: 'POST',
+                    headers: { ...authHeader(), 'Content-Type': 'application/json'},
+                    body: JSON.stringify({selectedIds: selectedIds})
+                };
+                return fetch(`${config.apiUrl}/extract/downloadMir?projectId=${projectId}&screenId=${screenId}&unlocked=${unlocked}`, requestOptions)
+                // .then(res => res.blob()).then(blob => saveAs(blob, `DOWNLOAD_${screen}_${year}_${baseTen(month+1)}_${date}.xlsx`));
+                .then(responce => {
+                    if (responce.status === 401) {
+                        localStorage.removeItem('user');
+                        location.reload(true);
+                    } else if (responce.status === 400) {
+                        this.setState({
+                            downloadingTable: false,
+                            alert: {
+                                type: 'alert-danger',
+                                message: 'an error has occured'  
+                            }
+                        });
+                    } else {
+                        this.setState({
+                            downloadingTable: false
+                        }, () => responce.blob().then(blob => saveAs(blob, `DOWNLOAD_${screen}_${year}_${baseTen(month+1)}_${date}.xlsx`)));
+                    }
+                });
+            });
+        }
+    }
+
+    uploadTable(event){
         event.preventDefault();
         const { fileName, projectId, screenId } = this.state
         if(this.fileInput.current.files[0] && projectId && screenId && fileName) {
@@ -1297,7 +1292,7 @@ class MaterialIssueRecord extends React.Component {
                                 <form
                                     className="col-12"
                                     encType="multipart/form-data"
-                                    onSubmit={this.handleUploadFile}
+                                    onSubmit={this.uploadTable}
                                     onKeyPress={this.onKeyPress}
                                     style={{marginLeft:'0px', marginRight: '0px', paddingLeft: '0px', paddingRight: '0px'}}
                                 >
