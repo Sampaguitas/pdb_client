684f8d56350d3e8426a740df19c3627f7ae36a08
Sampaguitas <timothee.desurmont@sampaguitas.com>
2020-11-15 17:01:19 +0400

    changes

--- src/pages/account/user.jsx
+++ src/pages/account/user.jsx
@@ -3,6 +3,7 @@ import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { 
+    alertActions,
     accessActions,
     collitypeActions,
     docdefActions,
