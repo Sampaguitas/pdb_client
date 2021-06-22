5704df9368d5aa7719e11319366de501224e68bb
Sampaguitas <timothee.desurmont@sampaguitas.com>
2021-06-22 11:27:55 +0400

    changes

--- src/pages/project/inspection/releasedata.jsx
+++ src/pages/project/inspection/releasedata.jsx
@@ -1502,14 +1502,7 @@ class ReleaseData extends React.Component {
     downloadTable(event){
         event.preventDefault();
         const { projectId, screenId, screen, selectedIds, unlocked } = this.state;
-        if (_.isEmpty(selectedIds)) {
-            this.setState({
-                alert: {
-                    type: 'alert-danger',
-                    message: 'Select line(s) to be downloaded.'
-                }
-            });
-        } else if (projectId && screenId && screen) {
+        if (projectId && screenId && screen) {
             this.setState({
                 downloadingTable: true
             }, () => {
