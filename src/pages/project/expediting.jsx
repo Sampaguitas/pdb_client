606a0e30c495d8ed6b5541cdb31f7ac0ed3cfa8a
Sampaguitas <timothee.desurmont@sampaguitas.com>
2021-06-22 11:20:32 +0400

    changes

--- src/pages/project/expediting.jsx
+++ src/pages/project/expediting.jsx
@@ -1429,14 +1429,7 @@ class Expediting extends React.Component {
     downloadTable(event){
         event.preventDefault();
         const { projectId, screenId, screen, selectedIds, unlocked } = this.state;
-        if (_.isEmpty(selectedIds)) {
-            this.setState({
-                alert: {
-                    type: 'alert-danger',
-                    message: 'Select line(s) to be downloaded.'
-                }
-            });
-        } else if (projectId && screenId && screen) {
+        if (projectId && screenId && screen) {
             this.setState({
                 downloadingTable: true
             }, () => {
